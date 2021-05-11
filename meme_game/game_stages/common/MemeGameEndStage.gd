5419ad864aba11e3302d2f45432fd7e429e524b7
Harry Jacobs <harryjjacobs@googlemail.com>
2021-05-12 00:07:29 +0100

    add thmemes and "yeah" sound effects

--- meme_game/game_stages/common/MemeGameEndStage.gd
+++ meme_game/game_stages/common/MemeGameEndStage.gd
@@ -1,16 +1,16 @@
 extends "res://core/game_stages/common/GameStage.gd"
 
+const _celebration_delay = 0.3
+
+export var _celebratory_sound_effect: AudioStreamSample
+
 onready var _title = $Title
 onready var _icon_display = $PlayerIconDisplay
 
 func enter(params):
 	.enter(params)
 	var winners = _get_winners()
-	if len(winners) > 1:
-		_title.text = "And the winners are..."
-	else:
-		_title.text = "And the winner is..."
-	_icon_display.add_players(winners)
+	_show_winners(winners)
 	set_timeout(10.0, _parameters)
 
 func _get_winners():
@@ -24,6 +24,15 @@ func _get_winners():
 		else:
 			break
 	return winners
+
+func _show_winners(winners):
+	if len(winners) > 1:
+		_title.text = "And the winners are..."
+	else:
+		_title.text = "And the winner is..."
+	_icon_display.add_players(winners)
+	yield(get_tree().create_timer(_celebration_delay), "timeout")
+	_play_audio(_celebratory_sound_effect)
 
 static func _sort_descending_by_points(a, b):
 	return b.points < a.points
